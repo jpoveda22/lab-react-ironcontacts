82c1d74699800f84cd9cfb64a33f1d664c25226a
jpoveda22 <47684055+jpoveda22@users.noreply.github.com>
2020-02-11 22:54:24 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import contacts from './contacts.json';
+
+class App extends Component {
+
+  // constructor(props){
+  //   super(props)
+  //   this.state ={
+  //     fiveContacts: contacts.slice(0, 5)
+  //   }
+  // }
+
+  state = {
+    fiveContacts: contacts.slice(0, 5)
+  }
+
+  showFiveContacts = () => {
+    // console.log(this.state.fiveContacts)
+    return this.state.fiveContacts.map((eachContact, index) => {
+      return (
+        <tr key={index}>
+          <th><img src={eachContact.pictureUrl} alt={eachContact.name}/></th>
+          <th>{eachContact.name}</th>
+          <th>{eachContact.popularity}</th>
+        </tr>
+      )
+  })
+  }
+  
+  render() {
+    console.log("Render App.js")
+    console.log(this.state.fiveContacts)
+    return (
+      <div className="App">
+        <h1>Table</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.showFiveContacts()}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
+
